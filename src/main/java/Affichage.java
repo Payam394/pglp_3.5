80a0d679aade7a54ba63f3466b0a968038b4f198
payam394 <payam.ak394@gmail.com>
2020-03-30 18:19:55 +0200

    Ecrir les methodes de Affichage

--- src/main/java/Affichage.java
+++ src/main/java/Affichage.java
@@ -1,4 +1,16 @@
+import java.time.LocalDateTime;
 
 public class Affichage {
-
+	
+	
+	public void DebutDeMethode(String nomMethode) {
+		System.out.println(LocalDateTime.now() + ": Début de " + nomMethode);
+		
+	}
+	
+	public void FinDeMethode(String nomMethode) {
+		System.out.println(LocalDateTime.now() + ": Fin de " + nomMethode);
+		
+	}
+	
 }
