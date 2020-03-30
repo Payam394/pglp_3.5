a311cb3e7554be40016fb4fc618594e723295974
payam394 <payam.ak394@gmail.com>
2020-03-30 18:20:30 +0200

    completer

--- src/main/java/UneClasseMetier.java
+++ src/main/java/UneClasseMetier.java
@@ -1,4 +1,24 @@
 
-public class UneClasseMetier {
+public class UneClasseMetier extends Affichage{
+	
+	String Metier;
+	
+	
+	
+	public UneClasseMetier(String metier) {
+		Metier = metier;
+	}
+
+
+
+	public void uneMethodeMetier () {
+		this.DebutDeMethode(Metier);
+		
+		/*
+		 * Traitemenet metiers
+		 */
+		
+		this.FinDeMethode(Metier);
+	}
 
 }
